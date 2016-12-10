b75a32453bfe98981e41495974c23ea1451f3592
window <window@10.16.122.22>
2016-12-10 10:45:22 +0800

    采购入库

---
+++ src/main/java/ocr/inventorycenter/pharseinv/PharseInvConstant.java
@@ -0,0 +1,33 @@
+package ocr.inventorycenter.pharseinv;
+
+/**
+ * 现存量 常量类
+ * 
+ * @author LCL
+ *
+ */
+public class PharseInvConstant {
+
+	
+	// -----------------组件信息
+	public static String ComponentNameConstant = "pharseinv-mgr";
+	public static String ComponentBizObjectTypeConstant = "bp_pharseinv";
+	
+	
+	// -------------类变量
+
+	
+	// -----------------handler address
+	public static String CreateAddressConstant = "create";
+	public static String ModifyAddressConstant = "modify";
+	public static String QueryAddressConstant = "query";
+	public static String RemoveAddressConstant = "remove";
+	
+	
+	// -----------------状态变化
+	public static String CreatedStatus = "created"; //创建完毕
+	
+	
+	
+
+}
