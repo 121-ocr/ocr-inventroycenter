ad590cea360b028597c6350583c8c95227ac0381
window <window@window-PC>
2016-12-15 09:30:51 +0800

    重构

--- src/main/java/ocr/inventorycenter/pharseinv/PharseInvQueryHandler.java
+++ src/main/java/ocr/inventorycenter/pharseinv/PharseInvQueryHandler.java
@@ -1,40 +1,35 @@
 package ocr.inventorycenter.pharseinv;
 
-
+import io.vertx.core.json.JsonObject;
 import ocr.common.handler.SampleBillBaseQueryHandler;
 import otocloud.framework.app.function.AppActivityImpl;
+
 /**
  * 库存中心：采购入库-查询
  * 
  * @date 2016年11月20日
  * @author LCL
  */
-//业务活动功能处理器
-public class PharseInvQueryHandler  extends SampleBillBaseQueryHandler {
+// 业务活动功能处理器
+public class PharseInvQueryHandler extends SampleBillBaseQueryHandler {
 
 	public static final String ADDRESS = PharseInvConstant.QueryAddressConstant;
 
 	public PharseInvQueryHandler(AppActivityImpl appActivity) {
 		super(appActivity);
-		// TODO Auto-generated constructor stub
+
 	}
 
 	// 此action的入口地址
 	@Override
 	public String getEventAddress() {
-		// TODO Auto-generated method stub
+
 		return ADDRESS;
 	}
 
-	/**
-	 * 要查询的单据状态
-	 * 
-	 * @return
-	 */
-	public String getStatus() {
-		// TODO Auto-generated method stub
- 	return PharseInvConstant.CreatedStatus;
-		//return msgBody.getString("status");
+	public String getStatus(JsonObject msgBody) {
+
+		return PharseInvConstant.CreatedStatus;
 	}
 
 }
