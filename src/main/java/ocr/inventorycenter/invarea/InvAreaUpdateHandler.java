5362a73519a12adda9698ad2c6bba59aa6294872
window <window@10.16.122.12>
2017-01-09 11:27:21 +0800

    库存档案二期

---
+++ src/main/java/ocr/inventorycenter/invarea/InvAreaUpdateHandler.java
@@ -0,0 +1,55 @@
+package ocr.inventorycenter.invarea;
+
+
+import io.vertx.core.http.HttpMethod;
+import ocr.common.handler.SampleDocBaseHandler;
+import otocloud.common.ActionURI;
+import otocloud.framework.app.function.ActionDescriptor;
+import otocloud.framework.app.function.AppActivityImpl;
+import otocloud.framework.app.function.BizRootType;
+import otocloud.framework.app.function.BizStateSwitchDesc;
+import otocloud.framework.core.HandlerDescriptor;
+/**
+ * 库存中心：库区-修改
+ * 
+ * @date 2016年11月20日
+ * @author LCL
+ */
+//业务活动功能处理器
+public class InvAreaUpdateHandler  extends SampleDocBaseHandler {
+	
+	public InvAreaUpdateHandler(AppActivityImpl appActivity) {
+		super(appActivity);
+	}
+
+	/**
+	 * corecorp_setting.setting
+	 */
+	@Override 
+	public String getEventAddress() {
+		return InvAreaConstant.UPDATE_ADDRESS;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ActionDescriptor getActionDesc() {
+
+		ActionDescriptor actionDescriptor = super.getActionDesc();
+		HandlerDescriptor handlerDescriptor = actionDescriptor.getHandlerDescriptor();
+
+		// 外部访问url定义
+		ActionURI uri = new ActionURI(getEventAddress(), HttpMethod.POST);
+		handlerDescriptor.setRestApiURI(uri);
+
+		// 状态变化定义
+		BizStateSwitchDesc bizStateSwitchDesc = new BizStateSwitchDesc(BizRootType.BIZ_OBJECT, null, InvAreaConstant.UPDATE_STATUS);
+		bizStateSwitchDesc.setWebExpose(true); // 是否向web端发布事件
+		actionDescriptor.setBizStateSwitch(bizStateSwitchDesc);
+
+		return actionDescriptor;
+	}
+	
+	
+}
