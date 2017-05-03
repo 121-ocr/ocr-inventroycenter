61699c9990f939c9ab8ea773d88441ed5f49c979
pcitc <pcitc@pcitc-PC>
2017-05-03 21:51:03 +0800

    增加给收银系统的查询方法

---
+++ src/main/java/ocr/inventorycenter/stockonhand/StockOnHandCommonQueryHandler.java
@@ -0,0 +1,47 @@
+package ocr.inventorycenter.stockonhand;
+
+import io.vertx.core.json.JsonObject;
+import ocr.common.handler.SampleDocQueryHandler;
+import otocloud.framework.app.function.AppActivityImpl;
+import otocloud.framework.core.OtoCloudBusMessage;
+
+/**
+ * 为收银系统提供的查询商品的方法
+ * 
+ * @author wanghw
+ *
+ */
+public class StockOnHandCommonQueryHandler extends SampleDocQueryHandler {
+
+	public StockOnHandCommonQueryHandler(AppActivityImpl appActivity) {
+		super(appActivity);
+		// TODO Auto-generated constructor stub
+	}
+
+	// 处理器
+	@Override
+	public void handle(OtoCloudBusMessage<JsonObject> msg) {
+		
+		JsonObject query = msg.body();
+
+		appActivity.getAppDatasource().getMongoClient().find(appActivity.getDBTableName(appActivity.getBizObjectType()),
+				query,
+				result -> {
+					if (result.succeeded()) {
+						msg.reply(result.result());
+					} else {
+						Throwable errThrowable = result.cause();
+						String errMsgString = errThrowable.getMessage();
+						appActivity.getLogger().error(errMsgString, errThrowable);
+						msg.fail(100, errMsgString);
+					}
+				});
+	}
+
+	@Override
+	public String getEventAddress() {
+		// TODO Auto-generated method stub
+		return StockOnHandConstant.CommonQueryAddressConstant;
+	}
+
+}
